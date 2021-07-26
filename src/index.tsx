5ca070879fda1057b634f7b4a7dd6af264ce12cc
amandaflores <amandaflores.br@gmail.com>
2021-07-25 22:07:09 -0300

    dirbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import GlobalStyle from './globalStyle';
 
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <GlobalStyle/>
   </React.StrictMode>,
   document.getElementById('root')
 );
